e008667ff5c3d3bc9b88a7c67085e6d772157187
xenonyx <aarav.rajput2003@gmail.com>
2021-12-17 15:46:56 +0530

    Applied env vars

--- mozillaBackend/mozillaTask/settings.py
+++ mozillaBackend/mozillaTask/settings.py
@@ -9,21 +9,29 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
-
 from pathlib import Path
+
+import os
+import environ
+env = environ.Env(
+    DEBUG=(bool,False)
+)
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+
+environ.Env.read_env(env_file=os.path.join(BASE_DIR,".env"))
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-extjij_w7p#5odg5+3+t&38fp&8=jm^^*w!3v+&cimz-^dphz8'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env('DEBUG')
 
 ALLOWED_HOSTS = []
 
